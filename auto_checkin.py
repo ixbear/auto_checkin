3ec9975005665318c7aeefe49608f8dcfa622b56
ixbear <zk1987313@gmail.com>
2017-06-15 19:54:51 +0800

    Create auto_checkin.py

--- auto_checkin.py
+++ auto_checkin.py
@@ -16,12 +16,24 @@ browser = webdriver.Chrome()
 print "Visit %s ..." %(req_url)
 browser.get(req_url)
 
-browser.find_element_by_id("username").send_keys("xiakening")
-browser.find_element_by_id("password").send_keys("PASSWORD")
+browser.find_element_by_id("username").send_keys("zhukun22")
+browser.find_element_by_id("password").send_keys("Zk6238475*()")
 browser.find_element_by_class_name("formsubmit_btn").click()
 
 time.sleep(2)
-browser.find_element_by_class_name("step_close").click()
+#browser.find_element_by_class_name("step_close").click()
+def isElementExist(element):
+  flag=True
+  try:
+    browser.find_element_by_class_name(element)
+    return flag
+  except:
+    flag=False
+    return flag
+
+
+if isElementExist("step_close"): 
+  browser.find_element_by_class_name("step_close").click()
 
 browser.find_element_by_id("clockLink").click()
 
